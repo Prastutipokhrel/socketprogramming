1aa3f7305e6580cacc703565f0d3ca3ff26d8d3a
Prastuti Pokhrel <prasttutipokhrel@gmail.com>
2024-03-25 15:18:57 -0500

    added server.c

---
+++ server.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define PROTOPORT 5193
+#define BUFFER_SIZE 1024
+#define QLEN 5
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    portno = PROTOPORT;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+
+    listen(sockfd, QLEN);
+
+    clilen = sizeof(cli_addr);
+
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+
+        // Handle client requests here
+
+        close(newsockfd);
+    }
+
+    close(sockfd);
+    return 0;
+}
