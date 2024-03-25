68a6988b50ace3f02b40ea6974ef49c87f11d12f
Prastuti Pokhrel <prasttutipokhrel@gmail.com>
2024-03-25 15:11:12 -0500

    added client.c

---
+++ client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PROTOPORT 5193
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE];
+
+    if (argc < 3) {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(1);
+    }
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+        error("ERROR invalid server address");
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    // Perform FTP operations here
+
+    close(sockfd);
+    return 0;
+}
